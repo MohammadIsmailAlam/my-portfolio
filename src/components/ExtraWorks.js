95deea8a2f013fdbda04c0d1844b45d982073881
Mohammad Ismail Alam <ismailalam901@gmail.com>
2023-11-06 12:40:51 +0600

    new projects added in extra option

--- src/components/ExtraWorks.js
+++ src/components/ExtraWorks.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { IconButton, Icon } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import IMG1 from "../../src/assets/madious1.png";
-import IMG2 from "../../src/assets/repliq.png";
-import IMG3 from "../../src/assets/lws.png";
+import IMG1 from "../../src/assets/279862710-f0db01a6-9e4a-461c-ba7.jpg";
+import IMG2 from "../../src/assets/madious1.png";
+import IMG3 from "../../src/assets/repliq.png";
+import IMG4 from "../../src/assets/lws.png";
+import IMG5 from "../../src/assets/technext.PNG";
 
 //ExtraWork function
 const ExtraWork = () => {
@@ -15,8 +17,19 @@ const ExtraWork = () => {
   const soloProjects = [
     {
       id: 1,
-      title: "Mediusware Coding Test",
+      title: "Ollyo Coding Test",
       img: IMG1,
+      description:
+        "Responsive image gallery using React JS with the following features: reordering, deleting multiple images, and setting a feature image.",
+      technologies:
+        "React Js | Tailwind CSS | React Hook From",
+      link: "https://ollyo-gallery.vercel.app/",
+      github: "https://github.com/MohammadIsmailAlam/ollyo-task",
+    },
+    {
+      id: 2,
+      title: "Mediusware Coding Test",
+      img: IMG2,
       description:
         "There were two problems to solve. One is kind of TO-DO and another one is Data fetching from a api and to show data as requirements",
       technologies:
@@ -25,9 +38,9 @@ const ExtraWork = () => {
       github: "https://github.com/MohammadIsmailAlam/Mediusware-codingTest",
     },
     {
-      id: 2,
+      id: 3,
       title: "Reqile Coding Test",
-      img: IMG2,
+      img: IMG3,
       description:
         "This site is a E-Commerce platform admin based. You can add items in to cart. Removed a cart item.",
       technologies: "React | Tailwind CSS",
@@ -35,13 +48,22 @@ const ExtraWork = () => {
       github: "https://github.com/MohammadIsmailAlam/Reqile-Coding-Test",
     },
     {
-      id: 3,
+      id: 4,
       title: "Learn With Sumit Coding test",
-      img: IMG3,
+      img: IMG4,
       description: "URL shorter web application",
       technologies: "React | React Hooks | Tailwind CSS",
       link: "https://lws-url-shortener-task.vercel.app/",
       github: "https://github.com/MohammadIsmailAlam/LWS_url-shortener_task",
+    },
+    {
+      id: IMG5,
+      title: "Technext Coding test",
+      img: IMG5,
+      description: "Fatched data from api, search and sort by date, sucesses and faliour",
+      technologies: "React | React Hooks | Context | React States",
+      link: "https://technext-test.vercel.app/",
+      github: "https://github.com/MohammadIsmailAlam/Technext-test",
     },
   ];
 
@@ -70,7 +92,7 @@ const ExtraWork = () => {
       </div>
       {showProjects && (
         <>
-          <h2>Given Coding tests in few company's</h2>
+          <h2>Attempted few Coding tests</h2>
           <div className="container portfolio__container rainbow">
             {soloProjects.map((pro) => (
               <article className="portfolio__item" key={pro.id}>
